f0d2c6532ea2bcff6a3c846e44eb1f1c770a6ac0
gaok <dlrkdhr321@gmail.com>
2021-04-08 14:03:53 +0900

    feat: 사이드 메뉴 바 제작 및 기본 설정 - 메뉴 바 제작 - 메뉴 바 마우스 hover 효과 - 메뉴 클릭 시 아이콘과 텍스트의 색 변경 - 메뉴 클릭 시 페이지 전환 - 화면 좌측 상단에 로고 추가 - Make Your Set 페이지 생성 및 페이지 내 제목, 소제목, 아이콘, 배경 생성 - Training 페이지 생성 및 페이지 내 제목, 소제목, 아이콘, 배경 생성 - Fit Notes 페이지 생성 및 페이지 내 제목, 소제목, 아이콘, 배경 생성 - Calender 페이지 생성 및 페이지 내 제목, 소제목, 아이콘, 배경 생성 - 프로필 표시 circle 생성 - 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import MakeYourSet from "./pages/MakeYourSet";
+import Training from "./pages/Training";
+import FitNotes from "./pages/FitNotes";
+import Calender from "./pages/Calender";
+import SideMenubar from "./pages/menu/SideMenubar";
+import './css/App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='main-container'>
+          <SideMenubar />
+          <Route path="/makeyourset" exact={true} component={MakeYourSet} />
+          <Route path="/training" component={Training} />
+          <Route path="/fitnotes" component={FitNotes} />
+          <Route path="/calender" component={Calender} />
+      </div>
   );
-}
+};
 
 export default App;
