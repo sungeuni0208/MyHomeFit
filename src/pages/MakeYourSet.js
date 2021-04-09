7be4b4ebab24fc4c7966b75a8bf1086556afa725
sungeuni0208 <sungeuni0208@naver.com>
2021-04-10 00:17:16 +0900

    feat: Make your set 페이지 제작 -url 입력창 생성 -url 버튼 아이콘 적용 -동작 블록 생성 -동작 블록 hover 색상 변경 fix typo: 메뉴바 오타 수정 fix: index.css 배경색 수정

--- src/pages/MakeYourSet.js
+++ src/pages/MakeYourSet.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import '../css/MakeYourSet/MakeYourSet.css';
 import IconSet from '../images/icon_makeyourset.png';
+import Icon from '../images/url_send.png';
 
 const MakeYourSet = () => {
     return (
-        <div className="menu1-wrapper">
-            <div className="menu1-container">
-                <div className="menu1-title">
-                    <div>
-                        <h5>Only for you, Gaok</h5>
-                        <img src={IconSet}/>
-                    </div>
+        <div className="content">
+            <div className="menu1-wrapper">
+                <div className="menu1-container">
+                    <div className="menu1-title">
+                        <div>
+                            <h5>Only for you, Gaok</h5>
+                            <img src={IconSet}/>
+                        </div>
 
-                    <svg width="100" height="100">
-                        <circle cx="50" cy="50" r="50" fill="white"></circle>
-                    </svg>
-                </div>
-                <div className="menu1-small-title">
-                    <label>Enter Fitness Youtube Video URL.</label>
-                </div>
+                        <svg width="100" height="100">
+                            <circle cx="50" cy="50" r="50" fill="white"></circle>
+                        </svg>
+                    </div>
+                    <div className="menu1-small-title">
+                        <label>Enter Fitness Youtube Video URL.</label>
+                    </div>
+                        <div className="url-container">
+                            <label>URL</label>
+                            <input className="url-input"></input>
+                            <img src={Icon} className="url-button"
+                            onClick={(e)=>{
+                            }}></img>
+                        </div>
+                    </div>
+                    <div id="trapezoid"></div>
+                    <label>Make Your Fitness Set.</label>
             </div>
-            <div id="trapezoid"></div>
+            <div className="block-box">
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+            </div>
         </div>
     );
 };
