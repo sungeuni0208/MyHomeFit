860190adcd6c351121979298d5d88865461e6ef0
k8jisoo <61647360+k8jisoo@users.noreply.github.com>
2021-05-04 23:49:43 +0900

    fix: Training 페이지 수정 - 전체 컴포넌트 크기 축소 - Next 이미지와 박스 컴포넌트 간 구성 수정 - 사용자 영상 박스 컴포넌트와 전문가 영상 박스 컴포넌트 분리 - 다음 동작 박스 컴포넌트 position 수정

---
+++ src/components/Training/NextPose.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/Training/NextPose.css';
+
+const NextPose = ({background_color}) => {
+    return(
+        <div className="next-videos" style={{backgroundColor: background_color}}/>
+    )
+}
+
+export default NextPose;
